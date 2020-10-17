8ed2430e0d5148ef27d33535c96fe4d9b2f212e4
FionaAvila <fionaavila15@gmail.com>
2020-10-17 13:56:48 +0700

    Initial Commit

---
+++ src/if1/pkg10119013/latihan8/pecahan/if110119013Latihan8Pecahan.java
@@ -0,0 +1,30 @@
+/*
+ * NAMA  : Fiona Avila Putri
+ * NIM   : 10119013
+ * KELAS : IF1 2019/2020 (PBO1)
+ */
+
+
+
+package if1.pkg10119013.latihan8.pecahan;
+
+/**
+ *
+ * @author Fiona Avila
+ */
+
+public class if110119013Latihan8Pecahan {
+
+    public static void main(String[] args) {
+        
+        float f;
+        double d;
+        
+        f = 1234567890.123456789F;
+        d = 1_234_567_890.123456789;
+        
+        System.out.println("Float\t: " + f);
+        System.out.println("Double\t: " + d);
+    }
+    
+}
